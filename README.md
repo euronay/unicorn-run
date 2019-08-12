b9a4fe09a4f18087438b0999f81424b32ba275da
James Naylor <jamesnaylor85@gmail.com>
2019-08-12 13:10:13 +0100

    Initial Commit 🦄

---
+++ README.md
@@ -0,0 +1,6 @@
+# Unicorn Run
+
+Learning LOVE2D by writing a game for my daughters.
+
+Started following tutorial from here: 
+http://osmstudios.com/tutorials/love2d-platformer-tutorial-part-3-creating-and-loading-levels
---
+++ main.lua
@@ -0,0 +1,20 @@
+-- Pull in Gamestate from the HUMP library
+Gamestate = require 'libs.hump.gamestate'
+
+-- Pull in each of our game states
+local mainMenu = require 'gamestates.mainMenu'
+local gameLevel1 = require 'gamestates.gameLevel1'
+local pause = require 'gamestates.pause'
+
+function love.load()
+  Gamestate.registerEvents()
+  Gamestate.switch(gameLevel1)
+end
+
+function love.keypressed(key)
+  if key == "escape" then
+    love.event.push("quit")
+  elseif key == 'p' then
+    return Gamestate.push(pause) -- return to previous state
+  end
+end
