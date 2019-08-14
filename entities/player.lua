6bf36e9a9e62048f4cd697d87e587c8aa63dd9b3
James Naylor <jamesnaylor85@gmail.com>
2019-08-14 22:43:06 +0100

    Better jump and state tracking ⏫

--- entities/player.lua
+++ entities/player.lua
@@ -19,19 +19,19 @@ function player:init(world, x, y)
   -- Add our unique player values
   self.xVelocity = 0 -- current velocity on x, y axes
   self.yVelocity = 0
-  self.acc = 100 -- the acceleration of our player
-  self.maxSpeed = 200 -- the top speed
+  self.acc = 200 -- the acceleration of our player
+  self.maxSpeed = 500 -- the top speed
   self.friction = 20 -- slow our player down - we could toggle this situationally to create icy or slick platforms
-  self.gravity = 200 -- we will accelerate towards the bottom
+  self.gravity = 250 -- we will accelerate towards the bottom
 
     -- These are values applying specifically to jumping
   self.isJumping = false -- are we in the process of jumping?
   self.isGrounded = false -- are we on the ground?
   self.hasReachedMax = false  -- is this as high as we can go?
-  self.jumpTimerMax = 0.8
+  self.jumpTimerMax = 0.3
   self.jumpTimer = self.jumpTimerMax
-  self.jumpAcc = 500 -- how fast do we accelerate towards the top
-  self.jumpMaxSpeed = 1000 -- our speed limit while jumping
+  self.jumpAcc = 750 -- how fast do we accelerate towards the top
+  self.jumpMaxSpeed = 8.5 -- our speed limit while jumping
 
   -- State
   self.state = "falling"
@@ -87,6 +87,7 @@ end
 
 function player:update(dt)
   local prevX, prevY = self.x, self.y
+  local wasJumping = self.isJumping
   local wasGrounded = self.isGrounded
   local hadReachedMax = self.hasReachedMax
 
@@ -98,79 +99,80 @@ function player:update(dt)
   self.yVelocity = self.yVelocity + self.gravity * dt
 
 	if love.keyboard.isDown("left", "a") and self.xVelocity > -self.maxSpeed then
-		self.xVelocity = self.xVelocity - self.acc * dt
+    self.xVelocity = self.xVelocity - self.acc * dt
+    self:message("move")
 	elseif love.keyboard.isDown("right", "d") and self.xVelocity < self.maxSpeed then
-		self.xVelocity = self.xVelocity + self.acc * dt
+    self.xVelocity = self.xVelocity + self.acc * dt
+    self:message("move")
+  else
+    self:message("stop")
 	end
 
-  -- The Jump code gets a lttle bit crazy.  Bare with me.
+  --Jump code
+  self.jumpTimer = self.jumpTimer - (1 * dt)
   if love.keyboard.isDown("up", "w") then
-    if self.yVelocity < self.jumpMaxSpeed and not self.hasReachedMax then
-      --print("jumping vel:" .. self.yVelocity .. " max:" .. self.jumpMaxSpeed .. " reached: " .. tostring(self.hasReachedMax))
+
+    if not self.isJumping and self.isGrounded then -- Starting the jump
+      self.isJumping = true
+      self.isGrounded = false
       self.yVelocity = self.yVelocity - self.jumpAcc * dt
-    elseif math.abs(self.yVelocity) > self.jumpMaxSpeed then
-      --print("reached max vel:" .. self.yVelocity .. " max:" .. self.jumpMaxSpeed .. " reached: " .. tostring(self.hasReachedMax))
-      self.yVelocity = 0
-      self.hasReachedMax = true
+      self.jumpTimer = self.jumpTimerMax
+      self:message("jump")
+    elseif self.jumpTimer > 0 and self.isJumping then -- Still jumping but have not hit timer
+      print(2 .. "timer: " .. self.jumpTimer)
+      self.yVelocity = self.yVelocity - self.jumpAcc * dt
+    elseif self.isJumping and self.yVelocity < 0 then -- Jump timer has run out, so slow down
+      self.yVelocity = self.yVelocity + self.jumpAcc * dt
+    elseif self.isJumping then -- We've stopped, so stop jumping and let gravity take us down
+      self.isJumping = false
+      self:message("hitroof")
     end
-  
-    self.isGrounded = false -- we are no longer in contact with the ground
   end
 
 
-  -- self.jumpTimer = self.jumpTimer - (1 * dt)
-  -- if love.keyboard.isDown("up", "w") and not self.isJumping and self.isGrounded then -- when the player hits jump
-  --   self.isJumping = true
-  --   self.isGrounded = false
-  --   self.yVelocity = -self.jumpAcc * dt
-  --   jumpTimer = jumpTimerMax
-  -- elseif love.keyboard.isDown("up", "w") and self.jumpTimer > 0 and self.isJumping then
-  --   self.yVelocity = self.yVelocity + -self.jumpAcc * dt
-  -- elseif not love.keyboard.isDown("up", "w") and self.isJumping then -- if the player releases the jump button mid-jump...
-  --   if self.yVelocity < self.jumpMaxSpeed then -- and if the player's velocity has reached the minimum velocity (minimum jump height)...
-  --     self.yVelocity = self.jumpMaxSpeed -- terminate the jump
-  --   end
-  --   self.isJumping = false
-  --   self.jumpTimer = self.jumpTimerMax
-  -- end
-
   -- these store the location the player will arrive at should
-  local goalX = self.x + self.xVelocity
-  local goalY = self.y + self.yVelocity
+  local xTarget = self.x + self.xVelocity
+  local yTarget = self.y + self.yVelocity
 
   -- Move the player while testing for collisions
-  self.x, self.y, collisions, len = self.world:move(self, goalX, goalY, self.collisionFilter)
+  self.x, self.y, collisions, len = self.world:move(self, xTarget, yTarget, self.collisionFilter)
 
+  self.isGrounded = false
   -- Loop through those collisions to see if anything important is happening
   for i, coll in ipairs(collisions) do
-    if coll.touch.y > goalY then  -- We touched below (remember that higher locations have lower y values) our intended target.
+    if coll.touch.y > yTarget then  -- We touched below (remember that higher locations have lower y values) our intended target.
       self.hasReachedMax = true -- this scenario does not occur in this demo
       self.isGrounded = false
+      self:message("hitroof")
     elseif coll.normal.y < 0 then
       self.hasReachedMax = false
       self.isGrounded = true
+      self:message("hitground")
     end
   end
 
   -- Update states - from https://2dengine.com/?p=fsm
-  if self.isGrounded and not wasGrounded then
-    self:message("hitground")
-  end
-  if not self.isGrounded and wasGrounded then
-    self:message("fall")
-  end
-  if self.hasReachedMax and not hadReachedMax then
-    self:message("hitroof")
-  end
-  if self.xVelocity ~= 0 then
-    self:message("move")
-  end
-  if self.yVelocity < 0 then
-    self:message("jump")
-  end
-  if self.xVelocity == 0 then
-    self:message("stop")
-  end
+  -- if self.isGrounded and not wasGrounded then
+  --   self:message("hitground")
+  -- end
+  -- if not self.isGrounded and wasGrounded then
+  --   self:message("fall")
+  -- end
+  -- if self.hasReachedMax and not hadReachedMax then
+  --   self:message("hitroof")
+  -- end
+  -- -- if self.isJumping and not wasJumping then
+  -- --   self:message("jump")
+  -- -- end
+  -- if self.xVelocity ~= 0 then
+  --   self:message("move")
+  -- end
+  -- if self.yVelocity < 0 then
+  --   self:message("jump")
+  -- end
+  -- if self.xVelocity == 0 then
+  --   self:message("stop")
+  -- end
 
   self:updateState(dt)
 
--- libs/states.lua
+++ libs/states.lua
@@ -1,8 +1,53 @@
 local states = {}
 
 states.standing = {}
+function states.standing:message(agent, event)
+    if event == "fall" then
+        agent:setState("falling")
+    elseif event == "jump" then
+        agent:setState("jumping")
+    elseif event == "move" then
+        agent:setState("running")
+    end
+end
+
 states.running = {}
+function states.running:message(agent, event)
+    if event == "fall" then
+        agent:setState("falling")
+    elseif event == "jump" then
+        agent:setState("jumping")
+    elseif event == "stop" then
+        agent:setState("standing")
+    end
+end
+
 states.falling = {}
+function states.falling:message(agent, event)
+    if event == "hitground" then
+        local xm = agent.xVelocity
+        if xm == 0 then
+            agent:setState("standing")
+        else
+            agent:setState("running")
+        end
+    elseif event == "jump" then
+        agent:setState("jumping")
+    end
+end
+
 states.jumping = {}
+function states.jumping:message(agent, event)
+    if event == "hitroof" then
+        agent:setState("falling")
+    elseif event == "hitground" then
+        local xm = agent.xVelocity
+        if xm == 0 then
+            agent:setState("standing")
+        else
+            agent:setState("running")
+        end
+    end
+end
 
 return states
