7d7089299740a9ef93464b300eadfe92d9c55968
James Naylor <jamesnaylor85@gmail.com>
2019-08-27 13:33:08 +0100

    Star bounces up and down 💫
    That was way harder than I anticipated

--- entities/star.lua
+++ entities/star.lua
@@ -15,10 +15,16 @@ function star:init(world, x, y)
 
     Entity.init(self, world, x - (self.image:getWidth() / 2), y - (self.image:getHeight() / 2), self.image:getWidth(), self.image:getHeight())
 
+    self.elapsed = 0
+    self.anchorY = y
+
     self.world:add(self, self:getRect())
 end
 
 function star:update(dt)
+    -- bounces the start up and down
+    self.elapsed = self.elapsed + 2.5 * dt
+    self.y = self.anchorY + math.cos(self.elapsed) * 10
 end
 
 function star:draw()
